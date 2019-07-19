ada0b980d0f16a7ce02662c8c38e22e77e4a11f5
Jacob Ankney <crosswalkastronaut@Jacobs-MBP-2.app.byu.edu>
2019-07-19 16:28:34 -0600

    some endpoints working

--- server.js
+++ server.js
@@ -6,9 +6,16 @@ const usersCtrl=require('./usersCtrl')
 app.listen(PORT,()=>{
 console.log('app is running on ', PORT )
 })
-
+app.use(express.json())
 
 //ENDPOINTS
 
 
 app.get('/api/user', usersCtrl.getUser)
+app.get(`/api/user/:userId`, usersCtrl.getIndividual)
+app.get(`/api/admin`, usersCtrl.getAdmins)
+app.get(`/api/nonadmin`, usersCtrl.getNonAdmins)
+app.get(`/api/type/:userType`, usersCtrl.getByType)
+
+
+app.put(`/api/user/:userId`, usersCtrl.updateUser)
--- usersCtrl.js
+++ usersCtrl.js
@@ -3,10 +3,94 @@ const userData= require('./userData.json')
 module.exports={
 getUser: (req, res)=>{
 
-switch(req.query)
+if(!req.query){
+    res.status(200).send(userData)
+}
 
-res.status(200).send(userData)
+    let {age, email, favorites:favs}=req.query
+
+let foundUsers= []
+
+if(favs){
+
+    for(let i=0;i<userData.length; i++){
+        if(userData[i].favorites.find(el=>{
+            return el===favs
+        }))
+        foundUsers.push(userData[i])
+    }
+    
+    
+    
+    res.status(200).send(foundUsers)
+}
+if(email){
+
+let users=userData.filter(el=>{
+    return el.email===email
+})
+    
+    
+    res.status(200).send(users)
+}
+
+if(age){
+let users= userData.filter(el=>{
+    return el.age<age
+})
+
+res.status(200).send(users)
+}
+
+
+
+
 },
+getIndividual: (req, res)=>{
+    let {userId}=req.params
+
+    let user=userData.find(el=>{
+        return +el.id===+userId
+    })
+    if(user){
+
+        res.status(200).send(user)
+    }
+    res.sendStatus(404)
+}, 
+
+getAdmins: (req, res)=> {
+    let admins=userData.filter(el=>{
+        return el.type==='admin'
+    })
+    res.status(200).send(admins)
+
+}, 
+getNonAdmins: (req, res)=> {
+    let nonAdmins=userData.filter(el=>{
+        return el.type!=='admin'
+    })
+    res.status(200).send(nonAdmins)
+}, 
+getByType: (req, res)=> {
+    
+    let users= userData.filter(el=>{
+        return el.type===req.params.userType
+    })
+
+    res.status(200).send(users)
+}, 
+updateUser: (req, res)=> {
+    let {userId}=req.params
+    let index= userData.findIndex(el=>{
+        return +el.id===+userId
+    })
+
+    let newObj={...req.body, id}
+
+    userData.splice(index, 1, req.body)
+res.status(200).send(userData)
+}
 
 
 }
